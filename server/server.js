bce8905a9123022734e188509b17d23fe22035b1
Krishal23 <kanhakesharwani23@gmail.com>
2025-05-12 02:22:14 +0530

    server update

--- server/server.js
+++ server/server.js
@@ -26,22 +26,30 @@ app.use(json({ extended: false }));
 //routes
 app.use('/api/resumes', resumesRouter);
 app.use('/api/companies', companiesRouter);
-app.all('/{*any}', (req, res, next) => {
-  res.status(404).send('Not Found');
-});
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+
+// API 404 handler
+app.all('/api/*', (req, res) => {
+  res.status(404).send('API Not Found');
 });
 
-
+// Production static file serving
 if (process.env.NODE_ENV === 'production') {
   app.use(serveStatic('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(resolve(process.cwd(), 'client', 'build', 'index.html'));
   });
+} else {
+  // Final 404 handler for non-production
+  app.all('*', (req, res) => {
+    res.status(404).send('Not Found');
+  });
 }
 
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
